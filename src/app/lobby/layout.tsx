cd7e6ce3aeebea975cd37054679c281a96baa861
unknown <rohansharmarohansharma22@gmail.com>
2024-11-07 17:21:34 +0530

    first commit

---
+++ src/app/lobby/layout.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className="layout">{children}</div>;
+}
